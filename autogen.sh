1f06fd475374b5dcc1768b406b3900804e6993da
Kokan <kokaipeter@gmail.com>
2021-02-10 12:56:01 +0100

    autogen.sh: aclocal cli change
    aclocal --install requires a path to be specified via -I or omitting it.
    This resolves building in the container specified in .gitlab-ci.yml
    
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- autogen.sh
+++ autogen.sh
@@ -21,7 +21,7 @@ if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
 	echo "" >&2
 fi
 
-aclocal --install || exit 1
+aclocal  || exit 1
 autoreconf --verbose --force --install -Wno-portability || exit 1
 
 cd $olddir
